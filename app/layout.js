d569cf40129cd8f080bfba64060a3a5e4589a5c6
surendra-kumar-07 <sure6@gmail.com>
2024-04-22 10:03:03 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,30 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ogImg from '/ogimg.png'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "10th & 12th बोर्ड का रिजल्ट घोषित",
   description: "10th & 12th बोर्ड का रिजल्ट घोषित 2024 ...",
+  openGraph: {
+    title: 'जल्दी Result यहाँ देखे...',
+    description: '10th & 12th बोर्ड का रिजल्ट घोषित 2024 ...',
+    url: 'https://google.com',
+    siteName: "result.org",
+    type: 'website',
+    images: [
+      {
+        url: './ogimg.png',
+        alt: "Result"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <meta property="og:title" content="जल्दी Result यहाँ देखे..."/>
-<meta property="og:site_name" content="Result check"/>
-<meta property="og:url" content="htttps//:google.com"/>
-<meta property="og:type" content="website"/>
-<meta property="og:description" content="10th & 12th बोर्ड का रिजल्ट घोषित 2024 ..."/>
-<meta property="og:image" content={ogImg}/>
-<meta property="og:image" content=""/>
-
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
