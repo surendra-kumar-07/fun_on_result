48ce126177ad23b798a4be655d286f3dc9aeac36
surendra-kumar-07 <sure6@gmail.com>
2024-04-23 13:19:25 +0530

    add Web Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-[#3c3086]`}>
         {children}
-        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
